681ed90869097dd8f3fbdd10863769c8648430f0
bharti <sanketdorle@yahoo.com>
2024-04-10 09:34:26 +0530

    --- Auto Git Commit ---

--- studentapp-ui-pipeline.groovy
+++ studentapp-ui-pipeline.groovy
@@ -11,7 +11,7 @@ pipeline{
 
         stage ('build'){
             steps{
-                sh '/opt/apache-maven-3.9.6/bin/mvn'
+                sh '/opt/apache-maven-3.9.6/bin/ mvn clean package'
                 echo 'build stage'
             }
         }
