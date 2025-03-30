3c1f2d0bd293ac3d35d4fc21149fd92a71901fc2
bharti <bharti@gmail.com>
2025-03-30 23:21:07 +0530

    --- Auto Git Commit ---

--- terraformpipeline.groovy
+++ terraformpipeline.groovy
@@ -4,22 +4,6 @@ pipeline {
     environment {
         AWS_REGION = "us-east-1"
     }
-
-    stages {
-        stage('Assume IAM Role') {
-            steps {
-                withCredentials([string(credentialsId: 'aws-role-arn', variable: 'ROLE_ARN')]) {
-                    sh '''
-                    echo "Assuming IAM Role..."
-                    CREDENTIALS=$(aws sts assume-role --role-arn $ROLE_ARN --role-session-name JenkinsSession)
-                    export AWS_ACCESS_KEY_ID=$(echo $CREDENTIALS | jq -r '.Credentials.AccessKeyId')
-                    export AWS_SECRET_ACCESS_KEY=$(echo $CREDENTIALS | jq -r '.Credentials.SecretAccessKey')
-                    export AWS_SESSION_TOKEN=$(echo $CREDENTIALS | jq -r '.Credentials.SessionToken')
-                    '''
-                }
-            }
-        }
-
         stage('Terraform Init') {
             steps {
                 sh 'terraform init'
