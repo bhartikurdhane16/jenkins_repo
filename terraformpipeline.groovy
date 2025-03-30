09ef7c97554b11ce57cfd993be6b84ff9726efa1
bharti <bharti@gmail.com>
2025-03-30 23:10:13 +0530

    --- Auto Git Commit ---

--- terraformpipeline.groovy
+++ terraformpipeline.groovy
@@ -34,43 +34,4 @@ pipeline {
     }
 }
 
-        stage('Terraform Init') {
-            steps {
-                sh 'terraform init'
-            }
-        }
-
-        stage('Terraform Validate') {
-            steps {
-                sh 'terraform validate'
-            }
-        }
-
-        stage('Terraform Plan') {
-            steps {
-                sh 'terraform plan -out=tfplan'
-            }
-        }
-
-        stage('Manual Approval') {
-            steps {
-                input message: "Do you want to apply Terraform changes?", ok: "Apply"
-            }
-        }
-
-        stage('Terraform Apply') {
-            steps {
-                sh 'terraform apply -auto-approve tfplan'
-            }
-        }
-    }
-
-    post {
-        success {
-            echo 'Terraform deployment completed successfully!'
-        }
-        failure {
-            echo 'Terraform deployment failed. Check logs for errors.'
-        }
-    }
-}
+       
